2e6a11069604d61a3d8847e6d39b961f22ec1105
novus-alex <alexandrehachet1@gmail.com>
2021-07-25 20:34:16 +0200

    added write func

---
+++ examples/example_receiver.py
@@ -0,0 +1,22 @@
+import serialio
+
+class Serial(object):
+	def __init__(self, port, baudrate, timeout):
+		self.port = port
+		self.baudrate = baudrate
+		self.timeout = timeout
+		self._openPort()
+
+	def _openPort(self):
+		self.hComm = serialio.Serial(self.port, self.baudrate)		# Opening the port
+
+	def read(self):
+		data = serialio.read(self.hComm)							# Listening to serial port
+		splited = data.split() 										# To remove \r\n(\n)
+		return splited[0] 											# Returning the data
+
+	def write(self, msg):
+		serialio.write(self.hComm, msg)
+
+ser = Serial("COM3", 9600, 1)
+ser.write("test")
